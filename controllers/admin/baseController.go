f305aa99cefb97f3a4be5f978d8e694dbe9d1a24
YXY <614533604@qq.com>
2023-04-23 17:36:31 +0800

    增加登录界面 配置路由 验证码认证

--- controllers/admin/baseController.go
+++ controllers/admin/baseController.go
@@ -8,10 +8,17 @@ import (
 type BaseController struct {
 }
 
-func (con *BaseController) Success(c *gin.Context) {
-	c.String(http.StatusOK, "success")
+func (con *BaseController) Success(c *gin.Context, message string, redirectUrl string) {
+	c.HTML(http.StatusOK, "admin/public/success.html", gin.H{
+		"message":     message,
+		"redirectUrl": redirectUrl,
+	})
 }
 
-func (con *BaseController) Error(c *gin.Context) {
-	c.String(http.StatusOK, "failed")
+func (con BaseController) Error(c *gin.Context, message string, redirectUrl string) {
+
+	c.HTML(http.StatusOK, "admin/public/error.html", gin.H{
+		"message":     message,
+		"redirectUrl": redirectUrl,
+	})
 }
---
+++ controllers/admin/loginController.go
@@ -0,0 +1,60 @@
+package admin
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"neverGiveUp/models"
+)
+
+type LoginController struct {
+	BaseController
+}
+
+func (con LoginController) Login(c *gin.Context) {
+	c.HTML(http.StatusOK, "admin/login/login.html", gin.H{})
+}
+
+func (con LoginController) DoLogin(c *gin.Context) {
+	//接收参数
+	captchaId := c.PostForm("captchaId")
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+	verifyValue := c.PostForm("verifyValue")
+	fmt.Println(username, password)
+	//查询数据库
+	flag := models.VerifyCaptcha(captchaId, verifyValue)
+	if flag {
+		userinfoList := []models.Manager{}
+		models.DB.Where("username=? And password=?", username, password).Find(&userinfoList)
+		if len(userinfoList) > 0 {
+			//3、执行登录 保存用户信息 执行跳转
+			session := sessions.Default(c)
+			//注意：session.Set没法直接保存结构体对应的切片 把结构体转换成json字符串
+			userinfoSlice, _ := json.Marshal(userinfoList)
+			session.Set("userinfo", string(userinfoSlice))
+			session.Save()
+			con.Success(c, "登录成功", "/admin")
+
+		} else {
+			con.Error(c, "用户名或者密码错误", "/admin/login")
+		}
+	} else {
+		con.Error(c, "验证码验证失败", "/admin/login")
+	}
+}
+
+func (con LoginController) Captcha(c *gin.Context) {
+	id, b64s, err := models.MakeCaptcha()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err,
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"captchaId":    id,
+		"captchaImage": b64s,
+	})
+}
