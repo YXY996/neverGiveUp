0fc1931979b2b538e7f7715a372178a90696542e
YXY996 <yangxinyu996@gmail.com>
2023-04-28 21:41:10 +0800

    增加商品属性的增删改查接口

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.19
 require (
 	github.com/gin-contrib/sessions v0.0.5
 	github.com/gin-gonic/gin v1.9.0
+	github.com/hunterhug/go_image v1.0.1
 	github.com/mojocn/base64Captcha v1.3.5
 	github.com/pkg/errors v0.9.1
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	gopkg.in/ini.v1 v1.67.0
 	gorm.io/driver/mysql v1.5.0
-	gorm.io/gorm v1.24.7-0.20230306060331-85eaf9eeda11
+	gorm.io/gorm v1.25.0
 )
 
 require (
---
+++ models/goodCate.go
@@ -0,0 +1,23 @@
+package models
+
+type GoodsCate struct {
+	Id          int
+	Title       string
+	CateImg     string
+	Link        string
+	Template    string
+	Pid         int
+	SubTitle    string
+	Keywords    string
+	Description string
+	Sort        int
+	Status      int
+	AddTime     int
+	// 商品目录 存在子目录
+	GoodsCateItems []GoodsCate `gorm:"foreignKey:pid;references:Id"`
+}
+
+// 约束 数据库为下划线
+func (GoodsCate) TableName() string {
+	return "goods_cate"
+}
