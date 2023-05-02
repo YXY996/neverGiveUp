68a7caea2e49241e60b36f66fedd8ad05f7cb83a
YXY996 <yangxinyu996@gmail.com>
2023-05-02 20:13:11 +0800

    增加setting model

--- models/nav.go
+++ models/nav.go
@@ -4,9 +4,9 @@ type Nav struct {
 	Id        int
 	Title     string
 	Link      string
-	Position  int
-	IsOpennew int
-	Relation  string
+	Position  int    //导航所在页面位置  0顶部 1中部 2 底部
+	IsOpennew int    //是否新窗口打开
+	Relation  string //关联商品
 	Sort      int
 	Status    int
 	AddTime   int
